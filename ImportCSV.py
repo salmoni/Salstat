3140c377d3c65e186b9d5ce2b65fad6fc41f5aaf
salmoni <salmoni@gmail.com>
2014-04-12 03:20:38 +0100

    Parses, does all the extras and handles errors
    Parses CSV files with multiple delimiters (work needed on quotes),
    handles headers as requested and imports data from a specified row,
    handles multiple line endings + guesses right one if unknown, and
    shoots it off to a preview.
    
    Work needed on fixed-width files…. And a couple of checkboxes are
    redundant.

--- ImportCSV.py
+++ ImportCSV.py
@@ -23,6 +23,9 @@ import wx
 from wx.stc import *
 import wx.grid as gridlib
 
+################################################
+# Dialog to retrieve file name. Needed before we can do anything
+
 class GetFilename(object):
     def __init__(self, parent, startDir):
         dlg = wx.FileDialog(parent, message="Open a CSV file", defaultDir=startDir, \
@@ -30,7 +33,6 @@ class GetFilename(object):
                 Data file (*.dat)|*.dat|Any file (*.*)|*.*")
         #dlg.SetIcon(ico)
         if dlg.ShowModal() == wx.ID_OK:
-            #print "Selected ", dlg.GetPath()
             self.fileName = dlg.GetPath()
             if os.path.exists(self.fileName):
                 self.fileName
@@ -39,11 +41,14 @@ class GetFilename(object):
         else:
             self.fileName = None
 
+################################################
+# Panel for delimited files
+
 class PanelDelimiter(wx.Panel):
     def __init__(self, parent, fileName):
         wx.Panel.__init__(self, parent, -1)
         self.parent = parent
-        help_text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis."
+        help_text = "Delimited files are separated (delimited) by characters like a comma. Salstat lets you specify the delimiters you need as well as a header (variable names)."
         self.box = wx.StaticBox(self, -1, "Select which delimiters:",pos=(10,60),\
                 size=(540,125))
         t1 = wx.StaticText(self, -1, label=help_text, pos=(10,10))
@@ -57,6 +62,10 @@ class PanelDelimiter(wx.Panel):
         self.del_06 = wx.CheckBox(self.box, 760, "Text delimiter:",pos=(0,80))
         self.edit_01 = wx.TextCtrl(self.box, 761, pos=(235,44), size=(30,-1))
         self.edit_02 = wx.TextCtrl(self.box, 762, pos=(125,79), size=(45,-1))
+        self.del_02.SetValue(True)
+
+################################################
+# Panel for fixed width files
 
 class PanelFixedWidth(wx.Panel):
     def __init__(self, parent, fileName):
@@ -65,98 +74,86 @@ class PanelFixedWidth(wx.Panel):
         t1 = wx.StaticText(self, -1, label=help_text, pos=(10,10))
         t1.Wrap(545)
 
+################################################
+# Main class that binds it all together
+
 class ImportDialog(wx.Dialog):
     def __init__(self, startDir):
         wx.Dialog.__init__(self, None, title="Importing a CSV file",\
                 size=(600,470))
         self.FileName = GetFilename(self, startDir)
-        #self.allFont = wx.Font(12, wx.MODERN, wx.NORMAL, wx.NORMAL)
-        #print FileName.fileName
-        #fin = open(FileName.fileName, 'r')
-        #head = [fin.next() for x in xrange(12)]
         self.decider = wx.Notebook(self, -1, pos=(10,0), \
                 size=(580,240), style=wx.NB_TOP)
         self.p1 = PanelDelimiter(self.decider, self.FileName.fileName)
         self.p2 = PanelFixedWidth(self.decider, self.FileName.fileName)
         self.decider.AddPage(self.p1, "Delimited files")
         self.decider.AddPage(self.p2, "Fixed width files")
-        t1 = wx.StaticText(self,-1,label="Header row:",pos=(250,250))
-        t2 = wx.StaticText(self,-1,label="Import from row:",pos=(410,250))
-        self.headerRow = wx.SpinCtrl(self, -1, min=0,max=100, initial=0, \
-                pos=(340,250),size=(50,-1))
-        self.dataRow = wx.SpinCtrl(self,-1,min=1,max=100, initial=1, \
-                pos=(530,250),size=(50,-1))
+        t1 = wx.StaticText(self,-1,label="Import from row:",pos=(245,250))
+        self.headerRow = wx.CheckBox(self, 760, " Header on first row", \
+                pos=(430,250))
+        self.dataRow = wx.SpinCtrl(self,766,min=1,max=100, initial=1, \
+                pos=(365,250),size=(50,-1))
+        self.dataRow.SetValue(1)
         self.buttonImport = wx.Button(self,763,"Import", \
                 pos=(500,400),size=(70,-1))
         self.buttonCancel = wx.Button(self,764,"Cancel", \
                 pos=(405,400),size=(70,-1))
         self.grid = gridlib.Grid(self, -1, size=(560,100),pos=(20,290))
-        self.grid.HideRowLabels()
-        self.grid.CreateGrid(8,8)
+        #self.grid.HideRowLabels()
+        self.MaxRows = 100
+        self.MaxCols = 8
+        self.grid.CreateGrid(self.MaxRows,self.MaxCols)
         self.grid.SetColLabelSize(16)
+        self.grid.SetRowLabelSize(40)
         for i in range(8):
             self.grid.SetRowSize(i,16)
+            self.grid.SetColLabelValue(i, " ")
         self.gridFont = wx.Font(10, wx.MODERN, wx.NORMAL, wx.NORMAL)
         self.grid.SetDefaultCellFont(self.gridFont)
         lineEndChoices = ['Not sure','\\n (Unix)','\\r\\n (Windows)','\\r (old Mac)']
         t3 = wx.StaticText(self,-1,"Line ending:",pos=(20,250))
-        self.lineEnd = wx.Choice(self, 765,pos=(110,248),size=(120,-1),\
+        self.lineEnd = wx.Choice(self, 765,pos=(105,248),size=(120,-1),\
                 choices = lineEndChoices)
         self.AttemptPreview()
+        # It seems odd but it seems better for all events to be routed to the same method
         wx.EVT_CHECKBOX(self, 760, self.AttemptPreview)
         wx.EVT_TEXT(self, 761, self.AttemptPreview)
+        wx.EVT_TEXT(self, 762, self.AttemptPreview)
         wx.EVT_BUTTON(self, 764, self.CancelButton)
         wx.EVT_BUTTON(self, 763, self.ImportButton)
         wx.EVT_CHOICE(self, 765, self.AttemptPreview)
+        wx.EVT_SPINCTRL(self, 766, self.AttemptPreview)
 
     def AttemptPreview(self, event=None):
-        # set up CSV format
-        delim = b','
-        quote = b'"'
-        if self.p1.del_01.IsChecked():
-            delim = b'\t'
-        if self.p1.del_02.IsChecked():
-            delim = b','
-        if self.p1.del_03.IsChecked():
-            delim = b' '
-        if self.p1.del_04.IsChecked():
-            delim = b';'
-        val = self.p1.edit_01.GetValue()
-        if val != "":
-            delim = b'%s'%val
-            delim = delim[0]
-            print delim,len(delim)
-        #csv.register_dialect('CSV_import',delimiter=delim)
+        # retrieve data 
         data = []
-        """
-        with open(self.FileName.fileName,'rb') as fin:
-            reader = csv.reader(fin, delimiter=delim, quotechar=quote)
-            for row in reader:
-                data.append(row)
-        fin.close()
-        """
         fin = open(self.FileName.fileName,'r')
         data = fin.read()
         fin.close()
-        self.FillGrid2(data)
+        # populate preview
+        self.FillGrid(data)
 
-    def FillGrid2(self, data):
+    def FillGrid(self, data):
+        # Fill self.grid with data and variables
         self.grid.ClearGrid()
-        # work out most common line end
+        # Get line ending
         endSelect = self.lineEnd.GetSelection()
         lineTerm = '\n'
         if endSelect == 0:
+            # Use most common as a guess
             lineEnds = ['\n','\r\n','\r']
             maxes = []
             for lineEnd in lineEnds:
                 maxes.append(self.GetMax(data, lineEnd))
             lineTerm = lineEnds[lineEnds.index(max(lineEnds))-1]
+        # Or have user selection
         elif endSelect == 1:
             lineTerm = '\n'
         elif endSelect == 2:
             lineTerm = '\r\n'
         elif endSelect == 3:
             lineTerm = '\r'
+        # split file into rows
         data = data.split(lineTerm)
         # get delimiters
         delims = ''
@@ -168,22 +165,47 @@ class ImportDialog(wx.Dialog):
             delims += b' '
         if self.p1.del_04.IsChecked():
             delims += b';'
-        # split each row
-        for row in range(8):
+        val = self.p1.edit_01.GetValue()
+        if val != "":
+            delims += val
+        quotes = self.p1.edit_02.GetValue()
+        beginRow = self.dataRow.GetValue()
+        if self.headerRow.IsChecked():
+            # User's setting a row as a header for the grid
+            self.headers = self.ParseLine(data[beginRow-1], delims, quotes)
+            for idx in range(self.MaxCols):
+                try:
+                    self.grid.SetColLabelValue(idx, self.headers[idx])
+                except IndexError:
+                    self.grid.SetColLabelValue(idx," ")
+            bonus = 1
+        else:
+            for idx in range(self.MaxCols):
+                self.grid.SetColLabelValue(idx, " ")
+            bonus = 0
+            self.headers = None
+        # Fill the grid as specified
+        self.gridData = []
+        for row in range(self.MaxRows):
             try:
-                line = data[row]
-                tokens = self.ParseLine(line, delims, '"')
+                line = data[beginRow+bonus+row-1]
+                tokens = self.ParseLine(line, delims, quotes)
+                self.gridData.append(tokens)
                 k = len(tokens)
-                if k > 8:
-                    k = 8
-                for idxy in range(k):
-                    self.grid.SetCellValue(row,idxy,tokens[idxy])
+                for idxy in range(self.MaxCols):
+                    try:
+                        self.grid.SetCellValue(row,idxy,tokens[idxy])
+                    except IndexError:
+                        self.grid.SetCellValue(row,idxy," ")
             except IndexError:
                 break
-        
+
+    #def GetHeader(self, data, delims, quotes):
+
     def ParseLine(self, line, delims, quotes):
         """
-        Parses a line of text into components
+        Parses a line of text into components. This attempts to 
+        be a proper parser that can cope with multiple delimiters.
         """
         inQuote = False
         try:
@@ -197,8 +219,10 @@ class ImportDialog(wx.Dialog):
             char = line[idx]
             if char in quotes:
                 if inQuote:
-                    if char == inQuoteChar:
-                        pass
+                    if char in inQuoteChar:
+                        inQuote = False
+                        tokens.append(token)
+                        token = ''
                     else:
                         token += char
                 else:
@@ -212,54 +236,60 @@ class ImportDialog(wx.Dialog):
                     else:
                         pass
                 else:
-                    #print char
                     token += char
         if len(token) > 0:
             tokens.append(token)
         return tokens
 
     def GetMax(self, data, lineEnd):
+        # help method to find the maximum of each line ending
         return len(data.split(lineEnd))
 
-    def FillGrid(self, data):
-        self.grid.ClearGrid()
-        for idxx, row in enumerate(data):
-            k = len(row)
-            if k > 8:
-                k = 8
-            for idxy in range(k):
-                self.grid.SetCellValue(idxx,idxy,row[idxy])
-
     def CancelButton(self, event):
+        # let's get out!
         self.Close()
-        sys.exit() # delete this!
 
     def ImportButton(self, event):
         self.Close()
-        sys.exit() # delete this!
 
 
 ################################################
-# Classes just for testing this module
+# Function to control all this
 
-class MyFrame(wx.Frame):
-    """"""
+def ImportCSV(startDir):
+    """
+    Controls all the module.
+    Parameters: startDir: where to start the file dialog
+    Returns: 
+    filename: Path + File name selected by user
+    headers: Variable name (None if none selected)
+    data: data as a list of lists
+    """
+    dlg = ImportDialog(startDir)
+    if dlg.ShowModal():
+        variableNames = dlg.headers
+        data = None
+        return dlg.FileName.fileName, dlg.headers, dlg.gridData
+    else:
+        return None
+    dlg.Destroy()
 
-    #----------------------------------------------------------------------
-    def __init__(self):
-        """Constructor"""
-        wx.Frame.__init__(self, None, title="Example frame")
 
-        # show a MessageDialog
-        style = wx.OK|wx.ICON_INFORMATION
-        # create panel
-        #panel = MyPanel(self)
-
+################################################
+# Main routine for testing only!
 
 if __name__ == '__main__':
     startDir = os.path.expanduser("~")
     app = wx.App(False)
-    frame = ImportDialog(startDir)
-    frame.Show()
-    app.MainLoop()
+    x = ImportCSV(startDir)
+    print x[0]
+    try:
+        print x[1],len(x[1])
+    except TypeError:
+        print "No headers"
+    print x[2],len(x[2])
+    #x.Close()
+    #frame = ImportDialog(startDir)
+    #frame.Show()
+    app.MainLoop()  
 
