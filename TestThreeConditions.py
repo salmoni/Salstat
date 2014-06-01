832ef6b13b633f93174b920b49d43248c276c6f1
salmoni <salmoni@gmail.com>
2014-06-01 12:21:10 +0100

    TestThreeConditions - new module
    This module creates the interface dialog for 2 or more condition test &
    variable selection

---
+++ TestThreeConditions.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+
+import time, os, os.path
+import urlparse, urllib, codecs
+import wx
+import images
+import sys, numpy
+import AllRoutines
+
+
+class WithinSubjectsClass(wx.Panel):
+    def __init__(self, parent, columnList):
+        wx.Panel.__init__(self, parent, -1)
+        helpWithin = "Within-subjects tests are when participants only take part in one of the conditions."
+        t20 = wx.StaticText(self,-1,label=helpWithin,pos=(20,20))
+        t20.Wrap(620)
+        t21 = wx.StaticText(self,-1,"Select the test you want:",pos=(60,80))
+        t22 = wx.StaticText(self,-1,"Select your variables:",pos=(320,80))
+        self.test1 = wx.CheckBox(self,-1,"Analysis of variance",pos=(60,110))
+        self.test2 = wx.CheckBox(self,-1,"Friedman's test",pos=(60,140))
+        self.test3 = wx.CheckBox(self,-1,"Cochrane's Q test",pos=(60,170))
+        self.DVBox = wx.CheckListBox(self,-1,pos=(320,100),size=(300,120),choices=columnList)
+
+class BetweenSubjectsClass(wx.Panel):
+    def __init__(self, parent, columnList):
+        wx.Panel.__init__(self, parent, -1)
+        helpBetween = "Between-subjects tests are when participants take part in all conditions. Between-subjects tests are when participants take part in all conditions."
+        t10 = wx.StaticText(self,-1,label=helpBetween,pos=(20,20))
+        t10.Wrap(620)
+        t11 = wx.StaticText(self,-1,"Select the test you want:",pos=(60,80))
+        t12 = wx.StaticText(self,-1,"Select your independent variable:",pos=(320,80))
+        t13 = wx.StaticText(self,-1,"Select your dependent variable:",pos=(320,150))
+        self.test1 = wx.CheckBox(self,-1,"Analysis of variance",pos=(60,110))
+        self.test2 = wx.CheckBox(self,-1,"Kruskal Wallis test",pos=(60,140))
+        self.IVBox = wx.Choice(self,211, (324,100), (160,20), columnList)
+        self.DVBox = wx.Choice(self,212, (324,170), (160,20), columnList)
+
+class TestDialog(wx.Dialog):
+    def __init__(self, columnList, testFlag = False):
+        wx.Dialog.__init__(self, None, title="Analyse 2 or more conditions",\
+                size=(700,425))
+        self.testFlag = testFlag
+        helpTop = "These tests allow you to compare two or more conditions for significant differences. These tests allow you to compare two or more conditions for significant differences."
+        t1 = wx.StaticText(self,-1, label=helpTop, pos=(20,30),size=(-1,660))
+        t1.Wrap(660)
+        self.decider = wx.Notebook(self, -1, pos=(20,65), size=(660,290))
+        self.betweenSubs = BetweenSubjectsClass(self.decider, columnList)
+        self.withinSubs = WithinSubjectsClass(self.decider, columnList)
+        self.decider.InsertPage(0, self.betweenSubs,"Between-subjects")
+        self.decider.InsertPage(1, self.withinSubs,"Within-subjects")
+        okayButton = wx.Button(self,211,"Analyse",pos=(540,360),size=(125,-1))
+        cancelButton = wx.Button(self,212,"Cancel",pos=(390,360),size=(125,-1))
+        okayButton.SetDefault()
+        wx.EVT_BUTTON(self, 211, self.AnalyseButton)
+        wx.EVT_BUTTON(self, 212, self.CancelButton)
+
+    def AnalyseButton(self, event):
+        page = self.decider.GetSelection()
+        tests = []
+        if page == 0: # between-subjects test chosen
+            if self.betweenSubs.test1.IsChecked():
+                tests.append('anovabetween')
+            if self.betweenSubs.test2.IsChecked():
+                tests.append('kruskal')
+            val = self.betweenSubs.IVBox.GetSelection()
+            if val != wx.NOT_FOUND:
+                IV = [val]
+            val = self.betweenSubs.DVBox.GetSelection()
+            if val != wx.NOT_FOUND:
+                DV = [val]
+        elif page == 1: # within-subjects tests
+            if self.withinSubs.test1.IsChecked():
+                tests.append('anovawithin')
+            if self.withinSubs.test2.IsChecked():
+                tests.append('friedmans')
+            if self.withinSubs.test3.IsChecked():
+                tests.append('cochranes')
+            IV = []
+            DV = self.withinSubs.DVBox.GetChecked()
+        self.tests = test
+        self.IV = IV
+        self.DV = DV
+        self.Close()
+
+    def CancelButton(self, event):
+        self.Close()
+        if self.testFlag:
+            print "Test run - exiting now...\n"
+            sys.exit() # remove after testing
+        return None
+
+
+
+#---------------------------------------------------------------------------
+# main loop
+if __name__ == '__main__':
+    cList = ['a','b','c']
+    app = wx.App()
+    frame = TestDialog(cList, testFlag=True)
+    frame.Show(True)
+    app.MainLoop()
+
+    
